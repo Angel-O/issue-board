c2398b816c8380e92522c4663eab1f5d5cb7a1db
Angelo Oparah <aorhymes@gmail.com>
2021-07-21 16:35:33 +0100

    minor

--- back-end/src/main/scala/com/angelo/dashboard/programs/ZServerProgram.scala
+++ back-end/src/main/scala/com/angelo/dashboard/programs/ZServerProgram.scala
@@ -2,13 +2,13 @@ package com.angelo.dashboard.programs
 
 import com.angelo.dashboard.layers.ZAppLayers.ServerEnvironment
 import com.angelo.dashboard.services.ZHttpServer
-import zio.RIO
+import zio.URIO
 import zio.logging.Logging.info
 
 object ZServerProgram {
 
   /** A very minimal program. Use of the managed resource, handled by the ZLayer */
-  val serveHttpRequests: RIO[ServerEnvironment, Unit] =
+  val serveHttpRequests: URIO[ServerEnvironment, Unit] =
     ZHttpServer.service
       .tap(server => info(s"server created (secure: ${server.isSecure})"))
       .unit
